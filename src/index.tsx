749d59fea7edbea66eb262734a158376c5d868a6
ahyaemon <iamahyaemon@gmail.com>
2022-09-08 21:51:54 +0900

    svg を preload する場所を変更

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './App';
 import { Router } from '@solidjs/router';
+import {preloadImages} from "./utils/preload";
+
+preloadImages()
 
 render(
     () => <Router><App /></Router>,
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,14 +1,9 @@
-import {Component, onMount} from "solid-js";
+import {Component} from "solid-js";
 import classes from "./TestPage.module.css";
 import {Introduction} from "../components/Introduction";
 import {Exam} from "../components/Exam";
-import {preloadImages} from "../utils/preload";
 
 export const TestPage: Component = () => {
-
-    onMount(() => {
-        preloadImages()
-    })
 
     return (
         <div class={classes.container}>
