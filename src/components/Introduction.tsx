a674c5a4772d4a778e4f982d1287870f261c599d
ahyaemon <iamahyaemon@gmail.com>
2022-09-08 22:06:51 +0900

    最初の会話を二種類用意した

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,25 +4,43 @@ import {CommentIcon} from "../types/CommentIcon";
 import {CommentPosition} from "../types/CommentPosition";
 import classes from "./Introduction.module.css";
 
-const comments = [
-    {
-        icon: CommentIcon.obasan,
-        message: '数問で性格が分かるらしいわよ。とりあえずやってみたら？',
-        position: CommentPosition.left,
-    },
-    {
-        icon: CommentIcon.musume,
-        message: 'やなこった！',
-        position: CommentPosition.right,
-    },
-    {
-        icon: CommentIcon.obasan,
-        message: 'クソガキ！',
-        position: CommentPosition.left,
-    },
+type Comment = {
+    icon: CommentIcon
+    message: string
+    position: CommentPosition
+}
+
+type Conversation = Comment[]
+
+const conversations: Conversation[] = [
+    [
+        createObasanComment(CommentPosition.left, '数問で性格が分かるらしいわよ。とりあえずやってみたら？'),
+        createMusumeComment(CommentPosition.right, 'やなこった！'),
+        createObasanComment(CommentPosition.left, 'クソガキ！'),
+    ],
+    [
+        createObasanComment(CommentPosition.left, '質問に答えて性格を診断しましょう'),
+        createMusumeComment(CommentPosition.right, '当てはまる回答がない'),
+        createObasanComment(CommentPosition.left, 'それでも答えるのが大人なのよ'),
+    ],
 ]
 
+function createObasanComment(position: CommentPosition, message: string): Comment {
+    return { icon: CommentIcon.obasan, message, position }
+}
+
+function createMusumeComment(position: CommentPosition, message: string): Comment {
+    return { icon: CommentIcon.musume, message, position }
+}
+
+function getRandomConversation(): Conversation {
+    const i = Math.floor(Math.random() * conversations.length)
+    return conversations[i]
+}
+
 export const Introduction: Component = () => {
+
+    const comments = getRandomConversation()
 
     return (
         <div class={classes.container}>
