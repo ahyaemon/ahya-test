28a3e4395906a5aa0dc6b908b7eda59ed227be09
ahyaemon <iamahyaemon@gmail.com>
2022-09-08 09:55:49 +0900

    結果ページをデザインしてモック文章を追加

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 AHYA-TEST
 
 https://ahya-test.ahyaemon.com/
+
+### 使用した画像
+
+### favicon
+
+https://www.flaticon.com/free-logo/technology-logo_7413137?related_id=7413137
+
+#### png
+
+https://www.freeiconspng.com
+
+#### svg
+
+https://freesvg.org
--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,7 +1,27 @@
 import {Component} from "solid-js";
+import classes from "./ResultPage.module.css";
 
 export const ResultPage: Component = () => {
     return (
-        <div>result</div>
+        <div class={classes.container}>
+            <div style={{ "margin-top": "20px" }}>あなたの AHYA-TYPE は・・・</div>
+            <div style={{ "margin-top": "40px", "font-size": "24px", "font-weight": "bold", color: "#333" }}>赤いゴリラ</div>
+            <div style={{ "margin-top": "10px"}}>
+                <div class={classes.image}></div>
+            </div>
+            <div style={{ "margin-top": "40px"}} class={classes.detail}>
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+                赤いゴリラはとても足が速く、最高速度時速 200km で走ることもあると言われています。
+            </div>
+        </div>
     )
 }
