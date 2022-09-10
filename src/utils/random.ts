35285f6b794ee0fd706552a8149b9a553c8227cc
ahyaemon <iamahyaemon@gmail.com>
2022-09-10 13:00:57 +0900

    getRandom で同じ数字を連続で引かないようにした

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,9 +1,22 @@
 /**
  * ar の中からランダムで一つ取得する
+ *
+ * 直前の値と同じものは出さない（やりかたは適当）
  */
+let recently: number = -1
 export function getRandom<T>(ar: T[]) : T {
+    if (ar.length === 1) {
+        return ar[0]
+    }
+
     const i = Math.floor(Math.random() * ar.length)
-    return ar[i]
+
+    if (i === recently) {
+        return getRandom(ar)
+    } else {
+        recently = i
+        return ar[i]
+    }
 }
 
 /**
