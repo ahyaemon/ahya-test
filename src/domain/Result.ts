41f67318cca2db8f3b5f907bbc058d50d06bce5b
ahyaemon <iamahyaemon@gmail.com>
2022-09-10 20:56:56 +0900

    説明文を更新

--- src/domain/Result.ts
+++ src/domain/Result.ts
@@ -21,17 +21,15 @@ export class Result {
     get movementExplanation(): string {
         switch (this.movement) {
             case Movement.quit: return `
-                ${this.ahyaType}はあまり動くことを好まず、一日中同じ場所にいることもあります。
-                決して信念があって動いていない訳ではなく、ただ面倒だからという理由だけで同じ場所に居続けます。
+                ${this.ahyaType}タイプの人は出不精で、一日中家にいることもあります。
             `.trim()
             case Movement.walk: return `
-                ${this.ahyaType}は動きが遅く、1日で数メートルしか動かないこともあります。
+                ${this.ahyaType}タイプの人はマイペースで、何事も自分のペースで進めます。
                 ただ、命をおびやかすような非常事態や緊急事態の時には、テキパキと素早く行動します。
             `.trim()
             case Movement.run: return `
-                ${this.ahyaType}は足が速く、時速200kmで走ることもあると言われています。
-                常に動いていて、何事にも積極的です。
-                ただ、${this.ahyaType}タイプの人は周りから焦っているように見られることもあります。
+                ${this.ahyaType}タイプの人は常に何かしら行動していて、何事にも積極的です。
+                ただ、周りからは焦っているように見られることもあります。
             `.trim()
         }
     }
